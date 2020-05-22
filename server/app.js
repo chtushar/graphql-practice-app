3d7b367036301e4bacb9e6aa3e7ce409c3573ff2
chtushar <choudharit23@gmail.com>
2020-05-22 22:01:43 +0530

    init commit

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const mongoose = require("mongoose");
+const schema = require("./schema/schema");
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/gql-app", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("Connected to database");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
