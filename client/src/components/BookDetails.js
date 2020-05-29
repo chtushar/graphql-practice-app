e478b73e27e1fe287eebcf1dcbb7ac4c5eb602b2
Tushar Choudhari <choudharit23@gmail.com>
2020-05-29 14:00:28 +0530

    Done with the app

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -7,12 +7,20 @@ const BookDetails = ({ selected }) => {
     variables: { id: selected },
   });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Select a book...</p>;
+  if (loading) return <p id="book-details">Loading...</p>;
+  if (error) return <p id="book-details">Select a book...</p>;
 
   return (
-    <div className="book-details">
-      <p>{data.book.name}</p>
+    <div id="book-details">
+      <h2>{data.book.name}</h2>
+      <p>{data.book.genre}</p>
+      <p>{data.book.author.name}</p>
+      <p>All books by this author</p>
+      <ul className="other-books">
+        {data.book.author.books.map((item) => {
+          return <li key={item.id}>{item.name}</li>;
+        })}
+      </ul>
     </div>
   );
 };
